604037f2d0498bc7cbacce9d7d593bd910f8739b
K900 <me@0upti.me>
2023-11-27 20:59:37 +0300

    kdeconnect-kde: fix build

--- pkgs/kde/gear/kdeconnect-kde/default.nix
+++ pkgs/kde/gear/kdeconnect-kde/default.nix
@@ -1,11 +1,22 @@
 {
   lib,
   mkKdeDerivation,
+  qtmultimedia,
+  qtwayland,
+  pkg-config,
+  wayland,
+  wayland-protocols,
+  libfakekey,
 }:
 mkKdeDerivation {
   pname = "kdeconnect-kde";
 
+  extraNativeBuildInputs = [pkg-config];
+  extraBuildInputs = [qtmultimedia qtwayland wayland wayland-protocols libfakekey];
+
+  extraCmakeFlags = [
+    "-DQtWaylandScanner_EXECUTABLE=${qtwayland}/libexec/qtwaylandscanner"
+  ];
+
   meta.license = null;
-  # FIXME(qt5)
-  meta.broken = true;
 }
