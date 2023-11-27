c848b19c18292d3ba2a0ff27236ff0c346fb4b29
K900 <me@0upti.me>
2023-11-27 22:24:03 +0300

    kdeconnect: backport more qt6 patches

--- pkgs/kde/gear/kdeconnect-kde/default.nix
+++ pkgs/kde/gear/kdeconnect-kde/default.nix
@@ -2,6 +2,7 @@
   lib,
   mkKdeDerivation,
   fetchFromGitLab,
+  fetchpatch,
   qtconnectivity,
   qtmultimedia,
   qtwayland,
@@ -21,6 +22,19 @@ mkKdeDerivation {
     rev = "b19267598e6115a886fe34c4ff183141454dd2b8";
     hash = "sha256-H85p9e5jLU9FGwBEJXZ6z18Gi3Yfa2sp7pPN3MofDHY=";
   };
+
+  # more qt6 porting
+  patches = [
+    (fetchpatch {
+      url = "https://invent.kde.org/network/kdeconnect-kde/-/merge_requests/611.diff";
+      hash = "sha256-HiWCK8sxSIvbvfgXX25j9LYoI2uuwStChe3rqnX0KVY=";
+      excludes = ["CMakeLists.txt"];
+    })
+    (fetchpatch {
+      url = "https://invent.kde.org/network/kdeconnect-kde/-/merge_requests/612.diff";
+      hash = "sha256-asNIIBf9rfU4PcMUMzw8HJjm6uVWdXuU0IocNMHXNAI=";
+    })
+  ];
 
   extraNativeBuildInputs = [pkg-config];
   extraBuildInputs = [qtconnectivity qtmultimedia qtwayland wayland wayland-protocols libfakekey];
