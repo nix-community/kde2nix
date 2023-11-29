426159c134fee3f3eaeb6bae92ec0555b738f6e0
K900 <me@0upti.me>
2023-11-29 19:11:38 +0300

    kdiagram: unstable-2023-11-26 -> 3.0.0

--- pkgs/kde/misc/kdiagram/default.nix
+++ pkgs/kde/misc/kdiagram/default.nix
@@ -1,20 +1,17 @@
 {
   lib,
   mkKdeDerivation,
-  fetchFromGitLab,
+  fetchurl,
   qttools,
   qtsvg,
 }:
 mkKdeDerivation rec {
   pname = "kdiagram";
-  version = "unstable-2023-11-26";
+  version = "3.0.0";
 
-  src = fetchFromGitLab {
-    domain = "invent.kde.org";
-    owner = "graphics";
-    repo = pname;
-    rev = "b976be76242cc0cf2359817559df2c1b56551c43";
-    sha256 = "sha256-lxmkv0/bGwmzOy0d1YPBCQmc9agm+vFdymD/Tb8yuhY=";
+  src = fetchurl {
+    url = "mirror://kde/stable/${pname}/${version}/${pname}-${version}.tar.xz";
+    hash = "sha256-bV9T390BkBgVHAGToB7tNt8QERqSx8Bu19YxU16UPCE=";
   };
 
   extraNativeBuildInputs = [qttools qtsvg];
