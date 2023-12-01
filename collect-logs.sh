05fb91de6a36056ff7bb1ad4a9b64c98885b89b9
K900 <me@0upti.me>
2023-12-02 00:13:04 +0300

    collect-logs: faster

--- collect-logs.sh
+++ collect-logs.sh
@@ -3,5 +3,5 @@ set -eu
 
 mkdir -p logs
 for name in $(nix flake show --json --legacy | jq '.legacyPackages."x86_64-linux" | to_entries[] | select (.value.type == "derivation") | .key' -r); do
-    nix log ".#${name}" > "logs/${name}.log" || true
+    nix log ".#${name}" --no-warn-dirty --offline > "logs/${name}.log" || true
 done
