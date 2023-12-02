b6537531f5914a2557f86d89cf65f8e63a9316ec
K900 <me@0upti.me>
2023-12-02 11:55:17 +0300

    dolphin: fix kactivities reference

--- pkgs/kde/gear/dolphin/default.nix
+++ pkgs/kde/gear/dolphin/default.nix
@@ -1,4 +1,15 @@
-{mkKdeDerivation}:
+{
+  mkKdeDerivation,
+  fetchpatch,
+}:
 mkKdeDerivation {
   pname = "dolphin";
+
+  # Fix KActivities
+  patches = [
+    (fetchpatch {
+      url = "https://invent.kde.org/system/dolphin/-/commit/9c313813f58cc43acf973464c338b4de56156f1f.patch";
+      hash = "sha256-h9F7xKl80afOqJVlTYqkvgnboesy/+t0VMnkeo7zioU=";
+    })
+  ];
 }
