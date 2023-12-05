dda064804e828b7bc5e639ed6a3767da545d37f7
K900 <me@0upti.me>
2023-12-05 10:56:40 +0300

    plasma-workspace: cherrypick digital clock timezones fix

--- pkgs/kde/plasma/plasma-workspace/default.nix
+++ pkgs/kde/plasma/plasma-workspace/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   mkKdeDerivation,
+  fetchpatch,
   substituteAll,
   xorg,
   pkg-config,
@@ -26,6 +27,10 @@ mkKdeDerivation {
     (substituteAll {
       src = ./wallpaper-paths.patch;
       wallpapers = "${lib.getBin breeze}/share/wallpapers";
+    })
+    (fetchpatch {
+      url = "https://invent.kde.org/plasma/plasma-workspace/-/merge_requests/3646.diff";
+      hash = "sha256-gUti+VZuQgjVpjr3Oj6zbFra0tNyICFsG8aZRGdIv4g=";
     })
   ];
 
