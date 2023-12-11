05c7caf39199d7e21cea2df9c3fd69e3ee174596
K900 <me@0upti.me>
2023-12-11 10:36:56 +0300

    ksystemstats: pick patch to fix 100% CPU reporting

--- pkgs/kde/plasma/ksystemstats/default.nix
+++ pkgs/kde/plasma/ksystemstats/default.nix
@@ -1,5 +1,6 @@
 {
   mkKdeDerivation,
+  fetchpatch,
   pkg-config,
   libksysguard,
   networkmanager-qt,
@@ -8,6 +9,14 @@
 }:
 mkKdeDerivation {
   pname = "ksystemstats";
+
+  patches = [
+    # Fix 100% CPU reported as 0%
+    (fetchpatch {
+      url = "https://invent.kde.org/plasma/ksystemstats/-/commit/8e7cbc481f30c0155cd78a2d8ed89cee47c3be64.patch";
+      hash = "sha256-HHV3vo2VtGF4R10ln1g4jT80Cm6/x2yeYoF2gVpX4kI=";
+    })
+  ];
 
   extraNativeBuildInputs = [pkg-config];
   extraBuildInputs = [networkmanager-qt lm_sensors libnl];
