4c5e764442c4bc70dfb2fa58534421e1ed0d4747
K900 <me@0upti.me>
2023-12-12 23:27:15 +0300

    kio: cherry-pick patch to fix Okular KF6 build

--- pkgs/kde/frameworks/kio/default.nix
+++ pkgs/kde/frameworks/kio/default.nix
@@ -13,9 +13,15 @@ mkKdeDerivation {
     # Remove hardcoded smbd search path
     # FIXME(later): discuss with upstream?
     ./0001-Remove-impure-smbd-search-path.patch
+
+    # backports
     (fetchpatch {
       url = "https://invent.kde.org/frameworks/kio/-/commit/146db4f79413b3a904e5ff252ca61bb09e17be2c.diff";
       hash = "sha256-6tDHmGbRweauJJY8bNQMmRZKHQplhvyMN6On1SstXgM=";
+    })
+    (fetchpatch {
+      url = "https://invent.kde.org/frameworks/kio/-/commit/cbafb685bce8a7a09543d5fa3df286955cd6d5af.diff";
+      hash = "sha256-HkGgWEJFTj5flsZDEBxqzgo5c2tp8rj8BxA1kAqOhkc=";
     })
   ];
 
