f9a1d669d2983576590033e7369ad7dc0505951e
K900 <me@0upti.me>
2023-12-12 23:27:29 +0300

    okular: use KF6 branch

--- pkgs/kde/gear/okular/default.nix
+++ pkgs/kde/gear/okular/default.nix
@@ -1,6 +1,45 @@
-{mkKdeDerivation}:
+{
+  mkKdeDerivation,
+  fetchFromGitLab,
+  pkg-config,
+  qtspeech,
+  qtsvg,
+  kparts,
+  kxmlgui,
+  poppler,
+  libtiff,
+  libspectre,
+  libzip,
+  djvulibre,
+  ebook_tools,
+  discount,
+}:
 mkKdeDerivation {
   pname = "okular";
-  # FIXME(qt5)
-  meta.broken = true;
+  version = "unstable-2023-12-12";
+
+  src = fetchFromGitLab {
+    domain = "invent.kde.org";
+    owner = "graphics";
+    repo = "okular";
+    rev = "4ea50e078bad4ad4a1ce9e069cce1191e80616d0";
+    hash = "sha256-1NfKYQtFe8qZ2ByU73WXnPnl5qa7BAcSxOu6ZvvsWa8=";
+  };
+
+  extraNativeBuildInputs = [pkg-config];
+  extraBuildInputs = [
+    qtspeech
+    qtsvg
+
+    kparts
+    kxmlgui
+
+    poppler
+    libtiff
+    libspectre
+    libzip
+    djvulibre
+    ebook_tools
+    discount
+  ];
 }
