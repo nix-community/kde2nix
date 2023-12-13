aef4fe79a0546656d90e8a55345a5df13b2c94e5
K900 <me@0upti.me>
2023-12-13 10:30:38 +0300

    plasma-integration: split qt5 into its own output

--- pkgs/kde/plasma/plasma-integration/default.nix
+++ pkgs/kde/plasma/plasma-integration/default.nix
@@ -8,6 +8,8 @@
 mkKdeDerivation {
   pname = "plasma-integration";
 
+  outputs = ["out" "dev" "qt5"];
+
   # We can't add qt5 stuff to dependencies or the hooks blow up,
   # so manually point everything to everything. Oof.
   extraCmakeFlags = [
@@ -56,7 +58,7 @@ mkKdeDerivation {
 
   # Move Qt5 plugin to Qt5 plugin path
   postInstall = ''
-    mkdir -p $out/${libsForQt5.qtbase.qtPluginPrefix}/platformthemes
-    mv $out/${qtbase.qtPluginPrefix}/platformthemes/KDEPlasmaPlatformTheme5.so $out/${libsForQt5.qtbase.qtPluginPrefix}/platformthemes
+    mkdir -p $qt5/${libsForQt5.qtbase.qtPluginPrefix}/platformthemes
+    mv $out/${qtbase.qtPluginPrefix}/platformthemes/KDEPlasmaPlatformTheme5.so $qt5/${libsForQt5.qtbase.qtPluginPrefix}/platformthemes
   '';
 }
