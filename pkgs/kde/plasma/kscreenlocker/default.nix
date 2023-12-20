5905e03a81705e3868ba235e55001dd6a2f72890
K900 <me@0upti.me>
2023-12-21 00:30:54 +0300

    kscreenlocker: add qqc2-breeze-style dependency

--- pkgs/kde/plasma/kscreenlocker/default.nix
+++ pkgs/kde/plasma/kscreenlocker/default.nix
@@ -1,9 +1,10 @@
 {
   mkKdeDerivation,
   pam,
+  qqc2-breeze-style,
 }:
 mkKdeDerivation {
   pname = "kscreenlocker";
 
-  extraBuildInputs = [pam];
+  extraBuildInputs = [pam qqc2-breeze-style];
 }
