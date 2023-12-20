f38f7c55c8e9dfecba4b3c9f46c85ed26eb0f1ba
K900 <me@0upti.me>
2023-12-20 22:05:21 +0300

    gear: remove kio-extras-kf5 stub

--- pkgs/kde/gear/kio-extras-kf5/default.nix
+++
@@ -1,9 +0,0 @@
-# FIXME: figure out how to handle this
-{mkKdeDerivation}:
-mkKdeDerivation {
-  pname = "kio-extras-kf5";
-  outputs = ["out"];
-  buildCommand = "mkdir -p $out";
-  separateDebugInfo = false;
-  meta.description = "STUB";
-}
