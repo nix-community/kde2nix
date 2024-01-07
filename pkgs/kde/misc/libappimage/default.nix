533d015cbde417c9e7bef5fbf58694ef5eeaf862
K900 <me@0upti.me>
2024-01-08 00:47:13 +0300

    libappimage: backport GCC 13 fix

--- pkgs/kde/misc/libappimage/default.nix
+++ pkgs/kde/misc/libappimage/default.nix
@@ -3,6 +3,7 @@
   callPackage,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   cmake,
   autoconf,
   automake,
@@ -29,6 +30,15 @@ in
       rev = "v${version}";
       hash = "sha256-V9Ilo0zFo9Urke+jCA4CSQB5tpzLC/S5jmon+bA+TEU=";
     };
+
+    patches = [
+      # Fix build with GCC 13
+      # FIXME: remove in next release
+      (fetchpatch {
+        url = "https://github.com/AppImageCommunity/libappimage/commit/1e0515b23b90588ce406669134feca56ddcbbe43.patch";
+        hash = "sha256-WIMvXNqC1stgPiBTRpXHWq3edIRnQomtRSW2qO52TRo=";
+      })
+    ];
 
     postPatch = ''
       substituteInPlace cmake/libappimage.pc.in \
