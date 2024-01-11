898b4d16854491194cb3cafcba2759e9f3cc19a6
K900 <me@0upti.me>
2024-01-11 15:44:18 +0300

    ktextaddons: 1.5.2 -> 1.5.3

--- pkgs/kde/misc/ktextaddons/default.nix
+++ pkgs/kde/misc/ktextaddons/default.nix
@@ -2,29 +2,18 @@
   lib,
   mkKdeDerivation,
   fetchurl,
-  fetchpatch,
   qtspeech,
   qttools,
   kxmlgui,
 }:
 mkKdeDerivation rec {
   pname = "ktextaddons";
-  version = "1.5.2";
+  version = "1.5.3";
 
   src = fetchurl {
     url = "mirror://kde/stable/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-KIu7HO+x6LVwo4PHkuul/KKRxG3QFV28JTHwo2aEp9s=";
+    hash = "sha256-ilLbir+oqdaNLSkfsPi+IGWf14mZh7Tcr98kaNsJF9w=";
   };
-
-  # Backport build fix
-  # FIXME: remove when updated
-  patches = [
-    (fetchpatch {
-      url = "https://invent.kde.org/libraries/ktextaddons/-/commit/733f14eaeaf48a79b0016e5ce3ba237cd543241e.patch";
-      excludes = ["textspeechtotext/**"];
-      hash = "sha256-586KbHqvs5FjrlAdy+CM4F8YKslgbSo3qUEORw/TlJI=";
-    })
-  ];
 
   extraBuildInputs = [qtspeech qttools kxmlgui];
 
