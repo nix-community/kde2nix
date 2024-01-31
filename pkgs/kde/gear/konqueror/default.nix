6d668333b998048a40bc215b61c35a3ef3eaa4ce
K900 <me@0upti.me>
2024-01-31 18:54:36 +0300

    konqueror: add webenginedictconverter

--- pkgs/kde/gear/konqueror/default.nix
+++ pkgs/kde/gear/konqueror/default.nix
@@ -7,4 +7,8 @@ mkKdeDerivation {
   pname = "konqueror";
 
   extraBuildInputs = [qtwebengine hunspell];
+
+  extraCmakeFlags = [
+    "-DWebEngineDictConverter_EXECUTABLE=${qtwebengine}/libexec/qwebengine_convert_dict"
+  ];
 }
